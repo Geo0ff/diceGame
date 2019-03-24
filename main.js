6b0098e9d0fbd37d178db8a00f1fa31ab26fa261
Georgi Yanakiev <g.yanakev@yahoo.com>
2019-03-24 01:41:54 +0000

    changes

--- main.js
+++ main.js
@@ -62,7 +62,7 @@
          document.querySelector('.player-0-panel').classList.toggle('active');
          document.querySelector('.player-1-panel').classList.toggle('active');
 
-
+          //next player ternary operator 
         // document.querySelector('.player-0-panel').classList.remove('active');
         // document.querySelector('.player-1-panel').classList.add('active');
         
@@ -76,7 +76,10 @@
             roundScore =0;
             gamePlaying = true;
 
-            document.querySelector('.dice').style.display = 'none';
+            document.getElementById('dice1').style.display = 'none';
+            document.getElementById('dice2').style.display = 'none';
+
+           
             document.getElementById('score-0').textContent = '0';
             document.getElementById('score-1').textContent = '0';
             document.getElementById('current-0').textContent = '0';
@@ -87,7 +90,7 @@
             document.querySelector('.player-1-panel').classList.remove('winner');
             document.querySelector('.player-0-panel').classList.remove('active');
             document.querySelector('.player-1-panel').classList.remove('active');
-            document.querySelector('.player-1-panel').classList.add('active');
+            document.querySelector('.player-0-panel').classList.add('active');
         }
 
 
